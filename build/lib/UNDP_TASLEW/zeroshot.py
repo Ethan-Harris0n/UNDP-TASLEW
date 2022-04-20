0422672f1843e40e77b303a70eb4d1520bd76852
Ethan Harrison <eh0097@gmail.com>
2022-04-20 11:31:44 +0530

    updated zeroshot for multiclass and created example notebook for parul

--- build/lib/UNDP_TASLEW/zeroshot.py
+++ build/lib/UNDP_TASLEW/zeroshot.py
@@ -5,20 +5,23 @@ import pandas as pd
 from transformers import pipeline
 
 class zeroshot(object):
-    def __init__(self, text_col, labels, **pipeline_kwargs):
+    def __init__(self, text_col, labels, multilabel=False, **pipeline_kwargs):
             # self.df = df
             self.df = pd.DataFrame(text_col).set_index(text_col.index)
             self.text_col = text_col
             self.labels = labels
             self.classifier = pipeline("zero-shot-classification", **pipeline_kwargs)
+            self.multilabel = multilabel
 
     def add_labels(self, new_labels):
         self.labels.append(new_labels)
 
     def fit_model(self):
-        self.df['output'] = self.text_col.apply(lambda x:self.classifier(x, self.labels))
+        self.df['output'] = self.text_col.apply(lambda x:self.classifier(x, self.labels, multi_label=self.multilabel))
         self.df['labels'] = self.df['output'].apply(lambda x: x.get('labels'))
         self.df['scores'] = self.df['output'].apply(lambda x: x.get('scores'))
-        self.df.drop('output', axis=1)
-        self.df = self.df.apply(lambda x: x.explode() if x.name in ['labels','scores'] else x) # there's a more efficient way to do this / in the future probably should set up an if / else with multilabel user-specified argument
-        return self.df
+        self.df.drop('output', axis=1, inplace=True)
+        output_df = self.df.apply(lambda x: x.explode() if x.name in ['labels','scores'] else x) # there's a more efficient way to do this / in the future probably should set up an if / else with multilabel user-specified argument
+        return output_df
+
+
